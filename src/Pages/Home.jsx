b3107bdd845446fb415e3e828a9bbde3cea6c751
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-04-07 21:33:04 +0500

    edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { TradingSignals } from "./FxTreding";
 export default function Home() {
     return (
         <div>
-            <TradingSignals symbols={["EUR/:FX", "GBP/JPY:FX",]} />
+            <TradingSignals symbols={["EUR/USD:FX", "GBP/JPY:FX",]} />
         </div>
     )
 }
